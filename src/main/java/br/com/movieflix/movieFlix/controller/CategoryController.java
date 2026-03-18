07beea86ac21aeb26ab95f706cb82e979d4a619a
Gabriel amaral <amaralgabriel357@gmail.com>
2026-03-17 22:03:13 -0300

    feat: configura o crud inicial do category

---
+++ src/main/java/br/com/movieflix/movieFlix/controller/CategoryController.java
@@ -0,0 +1,44 @@
+package br.com.movieflix.movieFlix.controller;
+
+import br.com.movieflix.movieFlix.model.CategoryModel;
+import br.com.movieflix.movieFlix.service.CategoryService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/movieflix/category")
+public class CategoryController {
+
+    private final CategoryService categoryService;
+
+    public CategoryController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
+    @GetMapping("/listar")
+    public List<CategoryModel> getAllCategories() {
+        return categoryService.findAll();
+    }
+
+    @PostMapping("/criar")
+    public CategoryModel saveCategory(@RequestBody CategoryModel categoryModel) {
+        return categoryService.saveCategory(categoryModel);
+    }
+
+    @GetMapping("/listarPorId/{id}")
+    public CategoryModel getCategoryById(@PathVariable Long id) {
+        Optional<CategoryModel> optCategory = categoryService.findById(id);
+        if (optCategory.isPresent()) {
+            return optCategory.get();
+        }
+        return null;
+    }
+
+    @DeleteMapping("/deletarPorId/{id}")
+    public void deleteCategoryById(@PathVariable Long id) {
+        categoryService.deleteCategory(id);
+    }
+
+}
--- src/main/java/br/com/movieflix/movieFlix/service/CategoryService.java
+++ src/main/java/br/com/movieflix/movieFlix/service/CategoryService.java
@@ -1,8 +1,32 @@
 package br.com.movieflix.movieFlix.service;
 
+import br.com.movieflix.movieFlix.model.CategoryModel;
+import br.com.movieflix.movieFlix.repository.CategoryRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CategoryService {
+
+    private final CategoryRepository categoryRepository;
+
+    public CategoryService(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    public List<CategoryModel> findAll() {
+        return  categoryRepository.findAll();
+    }
+    public CategoryModel saveCategory(CategoryModel categoryModel) {
+        return categoryRepository.save(categoryModel);
+    }
+    public Optional<CategoryModel> findById(Long id) {
+        return categoryRepository.findById(id);
+    }
+    public void deleteCategory(Long id) {
+        categoryRepository.deleteById(id);
+    }
 
 }
