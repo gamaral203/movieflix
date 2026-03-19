eeb8b66562c573056045bd1fb073f4b032fb9b52
Gabriel amaral <amaralgabriel357@gmail.com>
2026-03-19 18:16:32 -0300

    feat: Cria a extensão do JPA para persistência de dados

--- src/main/java/br/com/movieflix/movieFlix/repository/StreamingRepository.java
+++ src/main/java/br/com/movieflix/movieFlix/repository/StreamingRepository.java
@@ -1,4 +1,10 @@
 package br.com.movieflix.movieFlix.repository;
 
-public class StreamingRepository {
+import br.com.movieflix.movieFlix.model.StreamingModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StreamingRepository extends JpaRepository<StreamingModel, Long> {
+
 }
