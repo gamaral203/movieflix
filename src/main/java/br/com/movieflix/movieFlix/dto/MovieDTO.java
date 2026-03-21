4781642fb3f572f73050968043ab6d6aa921ac9f
Gabriel amaral <amaralgabriel357@gmail.com>
2026-03-21 14:33:55 -0300

    feat: cria e configura as classes MovieDTO e MovieMapper

---
+++ src/main/java/br/com/movieflix/movieFlix/dto/MovieDTO.java
@@ -0,0 +1,34 @@
+package br.com.movieflix.movieFlix.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class MovieDTO {
+
+    private Long id;
+
+
+    private String title;
+
+
+    private String description;
+
+
+    private LocalDate releaseDate;
+
+
+    private double rating;
+
+
+    private LocalDateTime createdAt;
+
+
+    private LocalDateTime updatedAt;
+}
---
+++ src/main/java/br/com/movieflix/movieFlix/mapper/MovieMapper.java
@@ -0,0 +1,31 @@
+package br.com.movieflix.movieFlix.mapper;
+
+import br.com.movieflix.movieFlix.dto.MovieDTO;
+import br.com.movieflix.movieFlix.model.MovieModel;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MovieMapper {
+
+    public MovieModel map(MovieDTO movieDTO) {
+        MovieModel movieModel = new MovieModel();
+        movieModel.setId(movieDTO.getId());
+        movieModel.setTitle(movieDTO.getTitle());
+        movieModel.setDescription(movieDTO.getDescription());
+        movieModel.setReleaseDate(movieDTO.getReleaseDate());
+        movieModel.setUpdatedAt(movieDTO.getUpdatedAt());
+        movieModel.setRating(movieDTO.getRating());
+        return movieModel;
+    }
+
+    public MovieDTO map(MovieModel movieModel) {
+        MovieDTO movieDTO = new MovieDTO();
+        movieDTO.setId(movieModel.getId());
+        movieDTO.setTitle(movieModel.getTitle());
+        movieDTO.setDescription(movieModel.getDescription());
+        movieDTO.setReleaseDate(movieModel.getReleaseDate());
+        movieDTO.setUpdatedAt(movieModel.getUpdatedAt());
+        movieDTO.setRating(movieModel.getRating());
+        return movieDTO;
+    }
+}
